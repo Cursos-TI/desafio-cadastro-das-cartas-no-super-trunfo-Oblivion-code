6fca70f44b82a01c99d4eb1ea90f4a7606c135fa
Oblivion-code <actp@outlook.com.br>
2025-03-08 17:08:31 +0000

    Desafio: nível mestre Completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ int main() {
     int numero_de_pontos_turisticos1, numero_de_pontos_turisticos2;
     float densidadePopulacional1, densidadePopulacional2;
     float PIBPercapta1, PIBPercapta2;
+    float superPoder1, superPoder2;
 
     // Recebimento das Informações da Primeira Carta utilizando o scanf
    
@@ -26,7 +27,7 @@ int main() {
     scanf ("%s", nome_da_cidade1);
 
     printf ("\nQual a População da Cidade?: \n");  // O scanf recebe um número inteiro e armazena em populacao1
-    scanf ("%u", &populacao1);
+    scanf ("%lu", &populacao1);
 
     printf ("\nQual a Área (em km²)?: \n "); // O scanf recebe um número decimal (float) e armazena em area_em_km21
     scanf ("%f", &area_em_km21);
@@ -40,6 +41,9 @@ int main() {
     // Cálcular a densidade populacional e o PIB per capita. (Nível Veterano)
     densidadePopulacional1 = (populacao1 / area_em_km21);
     PIBPercapta1 = (PIB1 / populacao1 );
+
+    //Cálcular o "super poder" (Nível Mestre)
+    superPoder1 = populacao1 + area_em_km21 + PIB1 + numero_de_pontos_turisticos1 + PIBPercapta1 + (1 / densidadePopulacional1);
 
     printf ("\n--------------------------------------------------------------\n");
    
@@ -56,7 +60,7 @@ int main() {
     scanf ("%s", nome_da_cidade2);
 
     printf ("\nQual a População da Cidade?: \n");
-    scanf ("%u", &populacao2);
+    scanf ("%lu", &populacao2);
 
     printf ("\nQual a Área (em km²)?: \n ");
     scanf ("%f", &area_em_km22);
@@ -71,6 +75,9 @@ int main() {
     densidadePopulacional2 = (populacao2 / area_em_km22);
     PIBPercapta2 = (PIB2 / populacao2 );
 
+    //Cálcular o "super poder" (Nível Mestre)
+    superPoder2 = populacao2 + area_em_km22 + PIB2 + numero_de_pontos_turisticos2 + PIBPercapta2 + (1 / densidadePopulacional2);
+
     printf ("\n--------------------------------------------------------------\n");
     
     // Exibindo as informações coletadas da Primeira Carta utilizando printf
@@ -79,7 +86,7 @@ int main() {
     printf ("Estado: %c \n", estado1);
     printf ("Código: %s \n", codigo_da_carta1);
     printf ("Nome da Cidade: %s \n", nome_da_cidade1);
-    printf ("População: %u \n", populacao1);
+    printf ("População: %lu \n", populacao1);
     printf ("Área: %.2f km² \n", area_em_km21);
     printf ("PIB: R$%.2f Bilhões \n", PIB1);
     printf ("Número de Pontos Turísticos: %d \n", numero_de_pontos_turisticos1);
@@ -94,12 +101,23 @@ int main() {
     printf ("Estado: %c \n", estado2);
     printf ("Código: %s \n", codigo_da_carta2);
     printf ("Nome da Cidade: %s \n", nome_da_cidade2);
-    printf ("População: %u \n", populacao2);
+    printf ("População: %lu \n", populacao2);
     printf ("Área: %.2f km² \n", area_em_km22);
     printf ("PIB: R$%.2f Bilhões \n", PIB2);
     printf ("Número de Pontos Turísticos: %d \n", numero_de_pontos_turisticos2);
     printf ("Densidade Populacional: %.2f de Pessoas por km² \n", densidadePopulacional2);
     printf ("PIB Per Capta: R$%.2f \n", PIBPercapta2);
+
+    // Comparando os atributos das cartas
+    
+    printf("\nComparação de Cartas:\n");
+    printf("População: Carta %d venceu (%d)\n", (populacao1 > populacao2) ? 1 : 2, (populacao1 > populacao2));
+    printf("Área: Carta %d venceu (%d)\n", (area_em_km21 > area_em_km22) ? 1 : 2, (area_em_km21 > area_em_km22));
+    printf("PIB: Carta %d venceu (%d)\n", (PIB1 > PIB2) ? 1 : 2, (PIB1 > PIB2));
+    printf("Pontos Turísticos: Carta %d venceu (%d)\n", (numero_de_pontos_turisticos1 > numero_de_pontos_turisticos2) ? 1 : 2, (numero_de_pontos_turisticos1 > numero_de_pontos_turisticos2));
+    printf("Densidade Populacional: Carta %d venceu (%d)\n", (densidadePopulacional1 < densidadePopulacional2) ? 1 : 2, (densidadePopulacional1 < densidadePopulacional2));
+    printf("PIB per Capita: Carta %d venceu (%d)\n", (PIBPercapta1 > PIBPercapta2) ? 1 : 2, (PIBPercapta1 > PIBPercapta2));
+    printf("Super Poder: Carta %d venceu (%d)\n", (superPoder1 > superPoder2) ? 1 : 2, (superPoder1 > superPoder2));
 
     return 0;
 }
